dc4863f88ac07cf4a71555c33891cb3d1e8e8b14
Dared-Evi1 <vborisin@mail.ru>
2024-10-18 18:44:54 +0500

    сделали 6 лабу

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -67,10 +67,6 @@ namespace Borisin_автосервис
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -99,6 +95,25 @@ namespace Borisin_автосервис
         {
             UpdateSerices();
 
+        }
+
+        private void AddButon_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                BorisinAutoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = BorisinAutoserviceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
